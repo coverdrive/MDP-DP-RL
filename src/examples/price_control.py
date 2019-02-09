fda465b8bc863565c48dff4acf6e1b2b1a734d2a
Ashwin Rao <ashwin.rao@gmail.com>
2019-02-08 19:27:27 -0800

    first version

--- src/examples/price_control.py
+++ src/examples/price_control.py
@@ -1,8 +1,11 @@
 import numpy as np
-T = 10
-M = 200
+T = 10  # time steps
+M = 200  # initial inventory
+# the following are (price, poisson mean) pairs, i.e., elasticity
 el = [(10.0, 10.0), (9.0, 16.0), (8.0, 20.0), (7.0, 23.0), (6.0, 25.0), (5.0, 26.0)]
 
+# v represents the Optimal Value Function (time, Inventory) -> E[Sum of Sales Revenue]
+# pi represents the Optimal Policy (time, Inventory) -> Price
 v = np.zeros((T + 1, M + 1))
 pi = np.zeros((T, M + 1))
 for t in range(T - 1, -1, -1):
