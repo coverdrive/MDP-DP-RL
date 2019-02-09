d980e784726819110c6fec87c7ca90b2862596c9
Ashwin Rao <ashwin.rao@gmail.com>
2019-02-08 19:06:04 -0800

    first version

---
+++ src/examples/price_control.py
@@ -0,0 +1,26 @@
+import numpy as np
+T = 10
+M = 200
+el = [(10.0, 10.0), (9.0, 16.0), (8.0, 20.0), (7.0, 23.0), (6.0, 25.0), (5.0, 26.0)]
+
+v = np.zeros((T + 1, M + 1))
+pi = np.zeros((T, M + 1))
+for t in range(T - 1, -1, -1):
+    for s in range(M + 1):
+        vals = np.zeros(len(el))
+        for i in range(len(el)):
+            p = el[i][0]
+            ld = el[i][1]
+            prob_sum = 0.
+            mult = 1.
+            for d in range(s):
+                if d > 1:
+                    mult *= d
+                prob = (np.exp(-ld) * ld ** d) / mult
+                vals[i] += prob * (d * p + v[t, s - d])
+                prob_sum += prob
+            vals[i] += (1. - prob_sum) * (s * p + v[t, s])
+        v[t, s] = np.max(vals)
+        pi[t, s] = el[int(np.argmax(vals))][0]
+print(pi)
+print(v)
