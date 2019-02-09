05d416b58b8a76b01700842bc0bd2f95286d2619
Ashwin Rao <ashwin.rao@gmail.com>
2019-02-08 19:46:02 -0800

    added types

--- src/examples/price_control.py
+++ src/examples/price_control.py
@@ -1,16 +1,18 @@
 import numpy as np
-T = 10  # time steps
-M = 200  # initial inventory
+from typing import Sequence
+T: int = 10  # time steps
+M: int = 200  # initial inventory
 # the following are (price, poisson mean) pairs, i.e., elasticity
-el = [(10.0, 10.0), (9.0, 16.0), (8.0, 20.0), (7.0, 23.0), (6.0, 25.0), (5.0, 26.0)]
+el: Sequence[(float, float)] = [(10.0, 10.0), (9.0, 16.0), (8.0, 20.0), (7.0, 23.0), (6.0, 25.0), (5.0, 26.0)]
 
 # v represents the Optimal Value Function (time, Inventory) -> E[Sum of Sales Revenue]
 # pi represents the Optimal Policy (time, Inventory) -> Price
-v = np.zeros((T + 1, M + 1))
-pi = np.zeros((T, M + 1))
+v: np.ndarray = np.zeros((T + 1, M + 1))
+pi: np.ndarray = np.zeros((T, M + 1))
+
 for t in range(T - 1, -1, -1):
     for s in range(M + 1):
-        vals = np.zeros(len(el))
+        vals: np.ndarray = np.zeros(len(el))
         for i in range(len(el)):
             p = el[i][0]
             ld = el[i][1]
@@ -25,5 +27,6 @@ for t in range(T - 1, -1, -1):
             vals[i] += (1. - prob_sum) * (s * p + v[t + 1, 0])
         v[t, s] = np.max(vals)
         pi[t, s] = el[int(np.argmax(vals))][0]
+
 print(pi)
 print(v)
