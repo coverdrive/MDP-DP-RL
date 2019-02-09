ccab0a9a58abcbe4d8580a040c5a11beaf9614fe
Ashwin Rao <ashwin.rao@gmail.com>
2019-02-08 21:23:56 -0800

    added types

--- src/examples/price_control.py
+++ src/examples/price_control.py
@@ -1,9 +1,16 @@
 import numpy as np
-from typing import Sequence
+from typing import Sequence, Tuple
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from matplotlib import cm
+
 T: int = 10  # time steps
 M: int = 200  # initial inventory
 # the following are (price, poisson mean) pairs, i.e., elasticity
-el: Sequence[(float, float)] = [(10.0, 10.0), (9.0, 16.0), (8.0, 20.0), (7.0, 23.0), (6.0, 25.0), (5.0, 26.0)]
+el: Sequence[Tuple[float, float]] = [
+    (10.0, 10.0), (9.0, 16.0), (8.0, 20.0),
+    (7.0, 23.0), (6.0, 25.0), (5.0, 26.0)
+]
 
 # v represents the Optimal Value Function (time, Inventory) -> E[Sum of Sales Revenue]
 # pi represents the Optimal Policy (time, Inventory) -> Price
@@ -30,3 +37,14 @@ for t in range(T - 1, -1, -1):
 
 print(pi)
 print(v)
+
+
+x, y = np.meshgrid(range(M + 1), range(T))
+fig = plt.figure()
+ax = fig.gca(projection='3d')
+surf = ax.plot_surface(x, y, pi, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+fig.colorbar(surf, shrink=0.5, aspect=5)
+plt.show()
+
+
