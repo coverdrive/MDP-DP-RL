7e3a2a5851f8ff75e384e1a8cb9f89e46d8826b3
Ashwin Rao <ashwin.rao@gmail.com>
2019-05-09 21:14:10 -0700

    lighter code

--- src/examples/price_control.py
+++ src/examples/price_control.py
@@ -1,39 +1,32 @@
 import numpy as np
 from typing import Sequence, Tuple
+from scipy.stats import poisson
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d.axes3d import Axes3D
 from matplotlib import cm
 
 T: int = 10  # time steps
-M: int = 200  # initial inventory
+M: int = 20  # initial inventory
 # the following are (price, poisson mean) pairs, i.e., elasticity
 el: Sequence[Tuple[float, float]] = [
-    (10.0, 10.0), (9.0, 16.0), (8.0, 20.0),
-    (7.0, 23.0), (6.0, 25.0), (5.0, 26.0)
+    (10.0, 0.3), (9.0, 0.8), (8.0, 1.6),
+    (7.0, 2.7), (6.0, 4.1), (5.0, 7.2)
 ]
 
 # v represents the Optimal Value Function (time, Inventory) -> E[Sum of Sales Revenue]
 # pi represents the Optimal Policy (time, Inventory) -> Price
 v: np.ndarray = np.zeros((T + 1, M + 1))
 pi: np.ndarray = np.zeros((T, M + 1))
+rvs: Sequence = [poisson(l) for _, l in el]
 
 for t in range(T - 1, -1, -1):
     for s in range(M + 1):
-        vals: np.ndarray = np.zeros(len(el))
-        for i in range(len(el)):
-            p = el[i][0]
-            ld = el[i][1]
-            prob_sum = 0.
-            mult = 1.
-            for d in range(s):
-                if d > 1:
-                    mult *= d
-                prob = (np.exp(-ld) * ld ** d) / mult
-                vals[i] += prob * (d * p + v[t + 1, s - d])
-                prob_sum += prob
-            vals[i] += (1. - prob_sum) * (s * p + v[t + 1, 0])
-        v[t, s] = np.max(vals)
-        pi[t, s] = el[int(np.argmax(vals))][0]
+        q_vals = [sum(rvs[i].pmf(d) * (d * p + v[t + 1, s - d])
+                    for d in range(s)) +
+                (1. - rvs[i].cdf(s - 1)) * s * p
+                for i, (p, _) in enumerate(el)]
+        v[t, s] = np.max(q_vals)
+        pi[t, s] = el[int(np.argmax(q_vals))][0]
 
 print(pi)
 print(v)
