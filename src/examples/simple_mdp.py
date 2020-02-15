e066b6cb2fa209515bcc0c82849770ad868b6717
Ashwin Rao <ashwin.rao@gmail.com>
2020-02-15 09:44:15 -0800

    added solutions to midterm problems

---
+++ src/examples/simple_mdp.py
@@ -0,0 +1,56 @@
+from processes.mdp_refined import MDPRefined
+from processes.det_policy import DetPolicy
+from typing import List, Mapping, Tuple, NoReturn
+
+
+def get_lily_pads_mdp(n: int) -> MDPRefined:
+    data = {
+        i: {
+            'A': {
+                i - 1: (i / n, 0.),
+                i + 1: (1. - i / n, 1. if i == n - 1 else 0.)
+            },
+            'B': {
+                j: (1 / n, 1. if j == n else 0.)
+                for j in range(n + 1) if j != i
+            }
+        } for i in range(1, n)
+    }
+    data[0] = {'A': {0: (1., 0.)}, 'B': {0: (1., 0.)}}
+    data[n] = {'A': {n: (1., 0.)}, 'B': {n: (1., 0.)}}
+
+    gamma = 1.
+    return MDPRefined(data, gamma)
+
+
+def get_sorted_q_val(
+    q_val: Mapping[int, Mapping[int, float]]
+) -> List[Tuple[float, float]]:
+    d = sorted([(s, (t['A'], t['B'])) for s, t in q_val.items()], key=lambda x: x[0])
+    return [z for _, z in d[1:-1]]
+
+
+def graph_q_func(a: List[Tuple[float, float]]) -> NoReturn:
+    import matplotlib.pyplot as plt
+    x_vals = range(1, len(a) + 1)
+    plt.plot(x_vals, [x for x, _ in a], "r", label="Q* for Action A")
+    plt.plot(x_vals, [y for _, y in a], "b", label="Q* for Action B")
+    plt.xlabel("Lilypad Number")
+    plt.ylabel("Value")
+    plt.title("Optimal Action Value Function")
+    plt.xlim(xmin=x_vals[0], xmax=x_vals[-1])
+    plt.ylim(ymin=0.45, ymax=0.55)
+    plt.xticks(x_vals)
+    plt.grid(True)
+    plt.legend(loc='lower right')
+    plt.show()
+
+
+if __name__ == '__main__':
+    pads: int = 100
+    mdp: MDPRefined = get_lily_pads_mdp(pads)
+    pol = mdp.get_optimal_policy(1e-8)
+    print(pol.policy_data)
+    print(mdp.get_value_func_dict(pol))
+    qv = mdp.get_act_value_func_dict(pol)
+    graph_q_func(get_sorted_q_val(qv))
---
+++ src/examples/wage_max.py
@@ -0,0 +1,75 @@
+from typing import Sequence, NamedTuple
+import numpy as np
+
+
+class WageMax(NamedTuple):
+
+    probs: Sequence[float]
+    wages: Sequence[float]
+    gamma: float
+    alpha: float
+    risk_aversion: float
+
+    def validate_inputs(self) -> bool:
+        b1 = sum(self.probs) == 1.
+        b2 = len(self.probs) + 1 == len(self.wages)
+        b3 = all(self.wages[0] < w for w in self.wages[1:])
+        b4 = 0. <= self.gamma < 1.
+        b5 = 0. <= self.alpha <= 1.
+        b6 = self.risk_aversion > 0.
+        return all([b1, b2, b3, b4, b5, b6])
+
+    # noinspection PyShadowingNames
+    def get_wages_utility(self) -> Sequence[float]:
+        a = self.risk_aversion
+        f = (lambda x, a=a: (pow(x, 1 - a) - 1) / (1 - a)) \
+            if a != 1 else (lambda x: np.log(x))
+        return [f(w) for w in self.wages]
+
+    def get_opt_vf(self) -> Sequence[float]:
+        jobs = len(self.probs)
+        utils = self.get_wages_utility()
+        vf = [0.] * (jobs + 1)
+        tol = 1e-4
+        epsilon = tol * 1e6
+        while epsilon >= tol:
+            old_vf = vf
+            vf[0] = max(
+                sum(self.probs[i] * utils[i + 1] for i in range(jobs)) +
+                    self.gamma * (self.alpha * vf[0] + (1 - self.alpha) *
+                    sum(self.probs[i] * vf[i + 1] for i in range(jobs))),
+                utils[0] + self.gamma * vf[0]
+            )
+            for i in range(1, jobs + 1):
+                vf[i] = utils[i] + self.gamma *\
+                            (self.alpha * vf[0] + (1 - self.alpha) * vf[i])
+            epsilon = max(abs(old_vf[i] - v) for i, v in enumerate(vf))
+        return vf
+
+    def get_opt_policy(self) -> Sequence[str]:
+        jobs = len(self.probs)
+        utils = self.get_wages_utility()
+        vf = self.get_opt_vf()
+        return ["Accept" if vf[i] > utils[0] + self.gamma * vf[0]
+                else "Decline" for i in range(1, jobs + 1)]
+
+
+if __name__ == '__main__':
+    this_probs: Sequence[float] = [0.5, 0.3, 0.2]
+    this_wages: Sequence[float] = [1.0, 1.8, 2.5, 5.2]
+    this_gamma: float = 0.98
+    this_alpha: float = 0.2
+    this_risk_aversion: float = 1.0
+    wm = WageMax(
+        probs=this_probs,
+        wages=this_wages,
+        gamma=this_gamma,
+        alpha=this_alpha,
+        risk_aversion=this_risk_aversion
+    )
+    if not wm.validate_inputs():
+        raise ValueError
+    opt_vf = wm.get_opt_vf()
+    opt_policy = wm.get_opt_policy()
+    print(opt_vf)
+    print(opt_policy)
